d75e7994f74873ca7873a1928fc975198a69c97b
quill_nichols <leplep00100@gmail.com>
2023-11-09 20:01:37 -0500

    git

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+
+let main = document.getElementById('main');
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let yourName = 'John Doe'; // Replace 'John Doe' with your desired text
+newHeader.textContent = yourName + ' is the champion';
+
 // Write your code here!
